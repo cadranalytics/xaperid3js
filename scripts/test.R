053a18f4683815f34d397432a28400b3f8b49584
flo12392 <flo12392@gmail.com>
2017-05-20 15:10:52 +0200

    added a install from github with auth token

---
+++ scripts/test.R
@@ -0,0 +1,11 @@
+# Install local version
+# setwd("~/Documents/GIT/xaperid3js")
+# devtools::install()
+
+# install version from github
+install_github("xaperi/xaperid3js",auth_token="5696500e9d9bba7406bdc1f952c45da20cf5bf8c")
+
+# test code
+# library("xaperid3js")
+# D3Gauge(0.8056,title="dit is een test titel", title2="dit is een extra titel")
+
