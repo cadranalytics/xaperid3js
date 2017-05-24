7e35dd22a3afec21ad539c6e6ba4fe616fb0bbf7
flo12392 <flo12392@gmail.com>
2017-05-24 09:27:12 +0200

    added reference to production

--- scripts/test.R
+++ scripts/test.R
@@ -3,9 +3,10 @@
 # devtools::install()
 
 # install version from github
-install_github("xaperi/xaperid3js",auth_token="5696500e9d9bba7406bdc1f952c45da20cf5bf8c")
+library(devtools)
+install_github("xaperi/xaperid3js",auth_token="5696500e9d9bba7406bdc1f952c45da20cf5bf8c",ref="production")
 
 # test code
-# library("xaperid3js")
+library("xaperid3js")
 # D3Gauge(0.8056,title="dit is een test titel", title2="dit is een extra titel")
 
